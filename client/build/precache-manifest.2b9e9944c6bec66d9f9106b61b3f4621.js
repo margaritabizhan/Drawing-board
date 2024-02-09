9adf4f21dd6897094ea1a6992d2e8fb94eb7f10f
margaritabizhan <margaritabizhan@gmail.com>
2024-02-09 17:59:34 -0500

    build client for deployment

---
+++ client/build/precache-manifest.2b9e9944c6bec66d9f9106b61b3f4621.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f901da8ab4a543ff5ec695fa364a8ace",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b36c56f800b2986da3c",
+    "url": "/static/css/main.dc24caff.chunk.css"
+  },
+  {
+    "revision": "b460362dbf6b9b0e7e00",
+    "url": "/static/js/2.49e8ebe9.chunk.js"
+  },
+  {
+    "revision": "022e8c402c3ae8df9fc93a871d76307b",
+    "url": "/static/js/2.49e8ebe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b36c56f800b2986da3c",
+    "url": "/static/js/main.10d0c47a.chunk.js"
+  },
+  {
+    "revision": "497cec80a04d1aecd33c",
+    "url": "/static/js/runtime-main.dc216f7b.js"
+  }
+]);
